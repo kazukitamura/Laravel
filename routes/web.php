13a9ab8cd6fb7f5f9efa3773046895dd33462f04
田村 <kazukitamura0905@gmail.com>
2019-07-13 04:30:20 +0000

    heroku 3commit

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-//Route::get('/', function () {
-    //return view('welcome');
-//});
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
      Route::get('news/create', 'Admin\NewsController@add');
@@ -33,5 +33,5 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/', 'NewsController@index');
+Route::get('/news', 'NewsController@index');
 Route::get('/profile', 'NewsController@profile');
